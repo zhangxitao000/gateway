0327bee0de23951507fb1365ea79229e5103b53e
zhangxitao <1034187483@qq.com>
2020-04-20 17:54:39 +0800

    简单网关

---
+++ src/main/java/com/gatewaydemo1/GatewaydemozuulApplication.java
@@ -0,0 +1,17 @@
+package com.gatewaydemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class GatewaydemozuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewaydemozuulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gatewaydemo1/Gatewaydemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.gatewaydemo1;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Gatewaydemo1ApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
